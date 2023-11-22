0f185f72ab63f00fa64e98acf213b3713bfc7bb3
VasilGVasilev <66898804+VasilGVasilev@users.noreply.github.com>
2023-11-22 14:35:06 +0200

    projects alpha done

--- website/components/MyProjects.js
+++ website/components/MyProjects.js
@@ -8,65 +8,54 @@ import { imageLoader } from "@/app/utils/imgLoader";
 
 
 
-const Project = ({ number, color, projectBorder, pictureLinkBorder, url, altLink, srcImg, imgW, imgH, projectName, projectDesc, githubLink }) => {
-  const overlayStyles = `mt-10 relative z-0 p-10 xl:p-16 before:absolute before:bottom-10 before:right-10
-  before:w-full before:h-full before:border-4 ${projectBorder} before:z-[-1]`;
-  const borderColor = `w-full h-fit border-t-4 border-r-4 ${pictureLinkBorder} m-5 overflow-hidden mx-auto`
+const Project = ({ url, altLink, srcImg, imgW, imgH, projectName, projectDesc }) => {
   return (
     <motion.div
-      className={overlayStyles}
+      className='p-10 grid grid-cols-1 sm:grid-cols-2 justify-center items-center gap-5'
       initial="hidden"
       whileInView="visible"
-      viewport={{ once: true, amount: 0.5 }}
-      transition={{ delay: 0.4, duration: 0.5 }}
+      viewport={{ once: true, amount: 0.2 }}
+      transition={{ duration: 0.5 }}
       variants={{
         hidden: { opacity: 0, y: 50 },
         visible: { opacity: 1, y: 0 },
       }}
     >
-      <div className="relative h-32">
-        <div className="z-10 absolute">
-          <Link href={url}>
-            <p className={`${playfair.className} font-semibold text-4xl hover:scale-125 transition`}>0{number}</p>
-            <p className={`${playfair.className} font-semibold text-2xl mt-3 hover:scale-125 transition`}>
-              {projectName}
-            </p>
-          </Link>
+      <img src="/dalia/dalia-one.webp" alt="" />
+      <div className="flex flex-col gap-3">
+        {/* Title */}
+        <div>
+          Далия
         </div>
-        <div className={`w-full h-36 bg-${color} absolute right-5 top-0 z-[1]`} />
-      </div>
-      <p className="mt-10 mr-5 text-justify text-neutral-300">
-        {projectDesc}
-      </p>
-      <br />
-      <Link href={githubLink} className="text-neutral-400 hover:text-white">See Github Code</Link>
-      <div className={borderColor}>
-        {/* The CSS OVERFLOW property controls what happens to content that is too big to fit into an area. */}
-        <Link href={url} >
-          <Image
-            className="transition-transform duration-700 ease-in-out hover:scale-150"
-            loader={imageLoader}
-            src={srcImg}
-            alt={altLink}
-            width={imgW}
-            height={imgH}
-          ></Image>
-        </Link>
+
+        {/* Desc */}
+        <div>
+          "Охридско езеро парк" е голям и модерен жилищен комплекс, разположен в идеалния център на гр. София, до парк "Възраждане". Комплексът е разположен на 111.6 хил. кв.м РЗП и се състои от 12 отделни сгради с различна етажност - от 1 до 17 етажа. Най-високата сграда - в северната част на парцела, е на 17 етажа, а другите две са терасовидни с височина от 9 до 13 етажа. Комплексът ще предложи 881 апартамента - половината от които с една спалня, а другите с по две, три и четири. Част от апартаментите са със собствени градини и гаражи. Предвидени са 982 автоместа, ресторант-градина, детски площадки и помещения за магазини.
+        </div>
+
+        {/* Done in */}
+        <div >
+          <div className="">
+            <p>Реализация</p>
+            <p>2025</p>
+          </div>
+          <Link href="#проекти" className="mt-10 inline-block bg-sky-dalia text-white px-3 py-4 font-semibold text-xl md:text-lg transition hover:bg-black hover:text-white">Разгледайте тук</Link>
+        </div>
       </div>
     </motion.div>
   );
 };
 
 const MyProjects = () => {
-
+  const isAboveLarge = useMediaQuery("(min-width: 1060px)");
   return (
     <section id="проекти" className="pt-10 pb-24 bg-white text-black">
       {/* HEADER AND IMAGE SECTION */}
-      <div className="md:flex md:justify-between md:gap-16 mt-32">
+      <div className="flex flex-col sm:flex-row sm:justify-evenly mt-10">
 
         {/* HEADER */}
         <motion.div
-          className="md:w-1/3"
+          className=""
           initial="hidden"
           whileInView="visible"
           viewport={{ once: true, amount: 0.5 }}
@@ -76,38 +65,45 @@ const MyProjects = () => {
             visible: { opacity: 1, x: 0 },
           }}
         >
-          <p className={`${playfair.className} font-semibold text-4xl md:text-6xl mb-5`}>
-            НАШИТЕ ПРОЕКТИ
+          <p className={`${playfair.className} font-semibold text-4xl md:text-6xl flex flex-col`}>
+            <span>НАШИТЕ</span>
+            <span>ПРОЕКТИ</span>
           </p>
-          <LineGradient width="w-11/12" />
+          <LineGradient />
 
         </motion.div>
 
         {/* IMAGE */}
         <motion.div
-          className="mt-16 md:mt-0"
+          className=""
           initial="hidden"
           whileInView="visible"
           viewport={{ once: true }}
-          transition={{ duration: 0.7, delay: 0.3 }}
+          transition={{ duration: 0.7 }}
           variants={{
-            hidden: { opacity: 0, translateY: 20, skewX: -80, rotateY: 50 },
-            visible: { opacity: 1, translateY: 0, skewX: 0, rotateY: 0 },
+            hidden: { opacity: 0, scale: 0 },
+            visible: { opacity: 1, scale: 1 },
           }}
         >
+          {isAboveLarge ? (
+            <div
+              className="relative z-0 ml-20 before:absolute before:-top-10 before:-left-10
+              before:w-full before:h-full before:border-2 before:border-black before:z-[-1]"
+            >
+              <img src="/crane.jpg" className="z-10 max-w-[600px] max-h-[600px]" alt="crane" />
 
+            </div>
+          ) : (
+            <></>
+          )}
         </motion.div>
       </div>
 
       {/* PROJECTS */}
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:justify-between mt-16 gap-10">
+      <div className="grid grid-cols-1 justify-center items-center mx-auto gap-5">
 
         {/* TILIA */}
         <Project
-          number='1'
-          color="red"
-          projectBorder="before:border-red"
-          pictureLinkBorder="border-red"
           url="https://tiliabg.com/"
           altLink="tilia-img"
           srcImg="/assets/project-tilia.webp"
@@ -115,15 +111,10 @@ const MyProjects = () => {
           imgH="2813"
           projectName={'Tilia Residence'}
           projectDesc={'A SSG website for a freelance project about sell of apartments developed via Next.js, Tailwind, React and Framer Motion. '}
-          githubLink={'https://github.com/VasilGVasilev/tilia'}
         />
 
         {/* IXORA */}
         <Project
-          number='2'
-          color="blue"
-          projectBorder="before:border-blue"
-          pictureLinkBorder="border-blue"
           url="https://ixorabg.com/"
           altLink="ixora-img"
           srcImg="/assets/project-ixora.webp"
@@ -131,15 +122,10 @@ const MyProjects = () => {
           imgH="2813"
           projectName={'Ixora Residence'}
           projectDesc={'A SSG website for a freelance project about sell of apartments developed via Next.js, Tailwind, React and Framer Motion. '}
-          githubLink={'https://github.com/VasilGVasilev/nextJS/tree/ixoraInter'}
         />
 
         {/* CHAT */}
         <Project
-          number='3'
-          color="red"
-          projectBorder="before:border-red"
-          pictureLinkBorder="border-red"
           url="http://vgvchat.tech/"
           altLink="chat-img"
           srcImg="/assets/project-chat.webp"
@@ -147,15 +133,10 @@ const MyProjects = () => {
           imgH="1087"
           projectName={'VGV Chat'}
           projectDesc={'A Create React App website for real-time messaging developed via SASS, React and Firebase with unit testing via Cypress.'}
-          githubLink={'https://github.com/VasilGVasilev/simpleChat'}
         />
 
         {/* AIRBNB */}
         <Project
-          number='4'
-          color="yellow"
-          projectBorder="before:border-yellow"
-          pictureLinkBorder="border-yellow"
           url="https://vgv-rental.vercel.app/"
           altLink="airbnb-img"
           srcImg="/assets/project-airbnb.webp"
@@ -163,7 +144,6 @@ const MyProjects = () => {
           imgH="1237"
           projectName={'Airbnb clone'}
           projectDesc={'A fullstack application developed via Typescript, Next.js, React, Tailwind, Prisma, MongoDB, Zustand, Next-Auth, brypt, axios.'}
-          githubLink={'https://github.com/VasilGVasilev/airbnb'}
 
         />
 
