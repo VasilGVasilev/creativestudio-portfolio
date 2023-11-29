f5540b45e50bdf9603d1a898e16e3ba3e5bb7eda
VasilGVasilev <66898804+VasilGVasilev@users.noreply.github.com>
2023-11-29 16:53:19 +0200

    add info

--- website/app/page.js
+++ website/app/page.js
@@ -1,8 +1,8 @@
 import Home from "./Home";
 
 export const metadata = {
-  title: 'Vasil Vasilev\'s portfolio',
-  description: 'Vasil Vasilev\'s portfolio website'
+  title: 'Creative Studio',
+  description: 'Creative Studio е строително-инвестиционно холдингово дружество, което финансира, проектира и изгражда луксозни многофамилни и еднофамилни жилищни сгради и ги реализира на пазара.'
 }
 
 export default function Page() {
